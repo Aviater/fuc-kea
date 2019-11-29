c244531a1c6560508888ac57140c44816f86bde7
Anna <anna.maria.wilczek@gmail.com>
2019-11-29 18:10:00 +0100

    change of default naming info fuc

--- src/test/java/com/project/fuc/kea/FucApplicationTests.java
+++ src/test/java/com/project/fuc/kea/FucApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.project.fuc.kea;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class FucApplicationTests {
 
     @Test
     void contextLoads() {
